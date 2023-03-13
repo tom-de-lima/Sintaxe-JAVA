057aad933b30db76da470054493a4953bf52ebf8
Tom_Lima <antoniojosedelimaneto@gmail.com>
2023-03-13 13:39:15 -0300

    Aula-tipos-variaveis

---
+++ src/Anatomia_de_Classes/AnatomiaClasses.java
@@ -0,0 +1,38 @@
+package Anatomia_de_Classes;
+
+public class AnatomiaClasses {
+    
+    public static void main(String[] args) { /*método único que tem a função de inicializar o programa */
+        
+        System.out.println("Olá Mundo!");
+
+        System.out.println(" ");
+
+        /* DECLARAÇÃO DE VARIÁVEIS: */
+
+        String meuNome = "Neto1";
+
+        int idade = 38;
+
+        boolean verdadeira = false;
+
+        idade = 39; /*manipulação de variável existente */
+
+        /*DECLARANDO MÉTODOS: */
+
+        String primeiroNome = "Lima";
+        String segundoNome = "Neto";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) { /*Declarado fora da main() */
+
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome); /*Observe as formas de concatenação ".concat() ou "+" */
+        
+    }
+
+}
---
+++ src/Tipos_de_Variaveis/Tipos_de_Variaveis.java
@@ -0,0 +1,9 @@
+package Tipos_de_Variaveis;
+
+public class Tipos_de_Variaveis {
+    
+public static void main(String[] args) {
+    
+}
+
+}
