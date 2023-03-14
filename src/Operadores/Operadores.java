5045b6af7465b5f91c1d1e8619fe5a878149c291
Tom_Lima <antoniojosedelimaneto@gmail.com>
2023-03-14 09:13:06 -0300

    Operadores

---
+++ src/Operadores/Operadores.java
@@ -0,0 +1,34 @@
+package Operadores;
+
+public class Operadores {
+    
+    public static void main(String[] args) {
+        
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+        /*REALIZANDO DEPURAÇÃO */
+
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        }
+
+}
